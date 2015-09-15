22e3fabf6c06955313d5887fed93618a096f2c35
Nikita Cherniavsky <mugi4ok@gmail.com>
2015-09-15 19:49:18 +0300

    Removed redundant folder

--- src/test/java/nca/Screens/VlNcaBy_TaxBaseOfLandTax.java
+++
@@ -1,7 +0,0 @@
-package nca.Screens;
-
-/**
- * Created by eviltech on 12.09.2015.
- */
-public class VlNcaBy_TaxBaseOfLandTax {
-}
--- src/test/java/nca/Screens/WekhfgNcaBy_IndexScreen.java
+++
@@ -1,7 +0,0 @@
-package nca.Screens;
-
-/**
- * Created by eviltech on 12.09.2015.
- */
-public class WekhfgNcaBy_IndexScreen {
-}
